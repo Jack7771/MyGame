62223d9cf5b325f06875b90de9ec1e74505bb61a
Jack7771 <Jack7771@live.com>
2017-02-14 16:09:29 +0000

    Create MainChar.java

---
+++ MainChar.java
@@ -0,0 +1,19 @@
+public class MainChar {
+  
+  private int left;
+  private int right;
+  
+  
+  public void leftMov() {
+      
+  }
+  
+  public void rightMov() {
+      
+  }
+  
+  public void vJump() {
+      
+  }
+
+}
